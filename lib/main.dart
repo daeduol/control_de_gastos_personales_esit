94a3746fdde237c6ca1db11c5b0cee4e72492fea
aviaud <viaudu@gmail.com>
2025-05-04 20:54:31 -0600

    Ulises
    Se agregó un widget para poder colocar fecha de los gastos.

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,26 @@
 import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
 
 void main() {
-  runApp(const MainApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("prueba de encabezado"),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Esit!'),
-            Text('hola.'),
-          ],
-          ),
-        ),
+      title: 'Mis Gastos',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
+        useMaterial3: true,
       ),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.system,
+      debugShowCheckedModeBanner: false,
+      home: const HomeScreen(),
     );
   }
 }
